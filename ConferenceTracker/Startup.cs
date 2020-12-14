5724c055b5f21755844b083242414a1fee216027
Thiago Cordeiro <thiago6891@gmail.com>
2020-12-14 13:53:09 -0300

    Setup distinct error pages for users and devs

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -39,8 +39,16 @@ namespace ConferenceTracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!env.IsDevelopment())
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/home/error");
                 app.UseHsts();
+            }
 
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
